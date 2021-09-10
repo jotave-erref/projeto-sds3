a9f5f36efd9b0f45ce5f2827ad4a3c18ba363147
Jean Victor <jvrferreira96@gmail.com>
2021-09-10 15:51:49 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsvendas.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
